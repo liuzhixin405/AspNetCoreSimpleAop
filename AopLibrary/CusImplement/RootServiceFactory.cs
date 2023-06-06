b3844f4c5a37153e5fa7c7289609d77aa49e537b
liuzhixin405 <164910441@qq.com>
2023-06-06 15:23:31 +0800

    'up'

--- AopLibrary/CusImplement/RootServiceFactory.cs
+++ AopLibrary/CusImplement/RootServiceFactory.cs
@@ -9,15 +9,15 @@ namespace AopLibrary.CusImplement
 {
     internal class RootServiceFactory<T> : IRootServiceFactory<T>
     {
-        private T _t { get; set; }
+        private T _instance { get; set; }
         public RootServiceFactory(T t, ISimpleAop aop)
         {
-            _t = t;
+            _instance = t;
             _aop = aop;
         }
 
         private ISimpleAop _aop { get; set; }
-        private T Imp => _t;
+        private T Imp => _instance;
 
         public async Task<TResponse?> Invoke<TResponse>(string methodName, object?[]? args)
         {
@@ -25,7 +25,7 @@ namespace AopLibrary.CusImplement
             {
                 throw new Exception("需要调用的方法名不能为空");
             }
-            var method = _t.GetType().GetMethods().Where(x => x.DeclaringType.Name == _t.GetType().Name).Where(x => x.Name.ToLower().Equals(methodName?.ToLower())).FirstOrDefault();
+            var method = _instance.GetType().GetMethods().Where(x => x.DeclaringType.Name == _instance.GetType().Name).Where(x => x.Name.ToLower().Equals(methodName?.ToLower())).FirstOrDefault();
             if (method == null)
             {
                 throw new Exception($"方法{methodName}不存在请检查");
@@ -43,7 +43,7 @@ namespace AopLibrary.CusImplement
             {
                 throw new Exception("需要调用的方法名不能为空");
             }
-            var method = _t.GetType().GetMethods().Where(x => x.DeclaringType.Name == _t.GetType().Name).Where(x => x.Name.ToLower().Equals(methodName?.ToLower())).FirstOrDefault();
+            var method = _instance.GetType().GetMethods().Where(x => x.DeclaringType.Name == _instance.GetType().Name).Where(x => x.Name.ToLower().Equals(methodName?.ToLower())).FirstOrDefault();
             if (method == null)
             {
                 throw new Exception($"方法{methodName}不存在请检查");
@@ -54,7 +54,7 @@ namespace AopLibrary.CusImplement
         private async Task<object?> InvokeCore(MethodInfo? targetMethod, object?[]? args)
         {
             await _aop.Before(args);
-            var result = targetMethod.Invoke(_t, args);
+            var result = targetMethod.Invoke(_instance, args);
             var afterResult = await _aop.After(result);
             return afterResult;
         }
