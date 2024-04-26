26cea3f32605d614c232e8911b2a860e3e08b6ba
164910441@qq.com <164910441@qq.com>
2024-04-26 20:09:41 +0800

    merge

--- AopLibrary/obj/Debug/netstandard2.1/AopLibrary.AssemblyInfo.cs
+++ AopLibrary/obj/Debug/netstandard2.1/AopLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AopLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d895b0728b7d6850169daec539c7e5588229015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b475e022bfe06e59630073b2492970159e2c29")]
 [assembly: System.Reflection.AssemblyProductAttribute("AopLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AopLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
