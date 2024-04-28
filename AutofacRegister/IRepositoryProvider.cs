3543fcb0ed1bf77ec3af90e19ce3b4fab2531528
liuzhixin405 <164910441@qq.com>
2024-04-28 12:01:43 +0800

    u
    u

--- AutofacRegister/IRepositoryProvider.cs
+++ AutofacRegister/IRepositoryProvider.cs
@@ -17,47 +17,55 @@ namespace AutofacRegister
     public class RepositoryProvider : IRepositoryProvider
     {
         private readonly Dictionary<string, (Assembly assembly, DateTime lastModified)> _assemblyCache = new Dictionary<string, (Assembly assembly, DateTime lastModified)>();
+        private readonly Dictionary<string, IRepository> _typeCache = new Dictionary<string, IRepository>();
 
-        public IRepository GetRepository(string x)
+        public IRepository GetRepository(string serviceName)
         {
-            var path = $"{Directory.GetCurrentDirectory()}\\lib\\{x}.Repository.dll";
-             var lastModified = File.GetLastWriteTimeUtc(path);
+            var path = $"{Directory.GetCurrentDirectory()}\\lib\\{serviceName}.Repository.dll";
+            var lastModified = File.GetLastWriteTimeUtc(path);
             if (_assemblyCache.TryGetValue(path, out var cachedEntry) && cachedEntry.lastModified == lastModified)
-        {
-            // 使用缓存中的 Assembly 对象
-            return CreateInstanceFromAssembly(cachedEntry.assembly);
-        }
-        else
-        {
-            // 加载并缓存新的 Assembly 对象
-            var assembly = LoadAssemblyFromFile(path);
-            _assemblyCache[path] = (assembly, lastModified);
-            return CreateInstanceFromAssembly(assembly);
-        }
+            {
+                // 使用缓存中的 Assembly 对象
+                return CreateInstanceFromAssembly(cachedEntry.assembly,serviceName);
+            }
+            else
+            {
+                // 加载并缓存新的 Assembly 对象
+                var assembly = LoadAssemblyFromFile(path);
+                _assemblyCache[path] = (assembly, lastModified);
+                return CreateInstanceFromAssembly(assembly,serviceName);
+            }
         }
 
- private Assembly LoadAssemblyFromFile(string path)
-    {
-        var _AssemblyLoadContext = new AssemblyLoadContext(Guid.NewGuid().ToString("N"), true);
-        using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+        private Assembly LoadAssemblyFromFile(string path)
         {
-            return _AssemblyLoadContext.LoadFromStream(fs);
+            var _AssemblyLoadContext = new AssemblyLoadContext(Guid.NewGuid().ToString("N"), true);
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                return _AssemblyLoadContext.LoadFromStream(fs);
+            }
         }
-    }
-        private IRepository CreateInstanceFromAssembly(Assembly assembly)
-    {
-        var type = assembly.GetTypes()
-            .Where(t => typeof(IRepository).IsAssignableFrom(t) && !t.IsInterface)
-            .FirstOrDefault();
-        
-        if (type != null)
+        private IRepository CreateInstanceFromAssembly(Assembly assembly,string serviceName)
         {
-            return (IRepository)Activator.CreateInstance(type);
+            var  type_key = $"{assembly.FullName}_{serviceName}";
+            if(_typeCache.TryGetValue(type_key, out var cachedType))
+            {
+                return _typeCache[type_key];
+            }
+            var type = assembly.GetTypes()
+                .Where(t => typeof(IRepository).IsAssignableFrom(t) && !t.IsInterface)
+                .FirstOrDefault();
+
+            if (type != null)
+            {
+                var instance= (IRepository)Activator.CreateInstance(type);
+                _typeCache[type_key] = instance;
+                return instance;
+            }
+            else
+            {
+                throw new InvalidOperationException("No suitable type found in the assembly.");
+            }
         }
-        else
-        {
-            throw new InvalidOperationException("No suitable type found in the assembly.");
-        }
-    }
     }
 }
