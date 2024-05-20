1ae10f62dfefb1e3bc81b77cfe4f922ae93a5890
164910441@qq.com <164910441@qq.com>
2024-05-20 21:35:14 +0800

    up

--- ModuleDevelop/ModuleLib/IModule.cs
+++ ModuleDevelop/ModuleLib/IModule.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ModuleLib
 {
     public interface IModule
     {
-        void ConfigureService(IServiceCollection services);
-        void Configure(IApplicationBuilder app);
+        void ConfigureService(IServiceCollection services, IConfiguration configuration=null);
+        void Configure(IApplicationBuilder app, IWebHostEnvironment env = null);
     }
 }
